48d6ad4be7c84ad0f2d77f5579bb91ba2a431ad9
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-08 10:12:25 -0400

    updated coding

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000, console.log(`listening on port 3000`))
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
+
+// npx nodemon js
