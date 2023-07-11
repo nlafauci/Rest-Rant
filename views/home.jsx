f1b4630c0e508c270a02ab96f7294d0196af9f48
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-11 11:08:08 -0400

    adding styles to rest-rant

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,11 @@ function home () {
           <main>
               <h1>HOME</h1>
           </main>
+          <a href="/places">
+              <button className="btn-primary">Places Page</button>
+          </a>
       </Def>
+      
     )
   }
   
