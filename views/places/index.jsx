9ca6e4fc93327768e8828271df2e41ebe4d8fe24
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-10 20:55:28 -0400

    minor edit

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,9 +2,9 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, i) => {
       return (
-        <div>
+        <div key={i}>
           <h2>{place.name}</h2>
           <img src={place.pic} alt={place.name}/>
         </div>
