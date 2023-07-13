a829c248f5256f767ed113ec58eb720d9ae433a3
nlafauci <nicholas.lafauci@gmail.com>
2023-07-13 09:52:03 -0400

    route connections

--- controllers/places.js
+++ controllers/places.js
@@ -8,6 +8,20 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id] })
+    }
+  })
+  
   
   router.post('/', (req, res) => {
     console.log(req.body)
