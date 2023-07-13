a7fb5b005f0b2269ac438f9307aa6e36174b941d
nlafauci <nicholas.lafauci@gmail.com>
2023-07-13 10:15:01 -0400

    delete route complete

--- controllers/places.js
+++ controllers/places.js
@@ -39,6 +39,19 @@ router.get('/new', (req, res) => {
     res.redirect('/places')
   })
    
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
   
 module.exports = router
 
