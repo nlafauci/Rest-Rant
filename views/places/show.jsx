4a3bd10018dd2f90c39867b55afa31178b32dcd6
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-23 12:08:02 -0400

    validations updated

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,10 +6,14 @@ function show (data) {
     <Def>
         <main>
             <h1>{ data.place.name }</h1>
-        </main>
-        <main>
+            <h3> Located in { data.place.city }, { data.place.state }</h3>
             <h2> Rating </h2>
             <p> Currently unrated </p>
+        </main>
+        <main>
+            <h2> Description </h2>
+            <h3> {data.place.showEstablished} </h3>
+            <h4> Serving {data.place.cuisines} </h4>
         </main>
         <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
         Edit
