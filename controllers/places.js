c9da5b0574eb95bef6c95baae156c4e7ddfe746a
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-26 15:29:03 -0400

    edited routes for mongo

--- controllers/places.js
+++ controllers/places.js
@@ -53,36 +53,39 @@ router.get('/:id', (req, res) => {
 
 
 router.put('/:id', (req, res) => {
-  db.Place.findByIdAndUpdate(req.params.id, req.body)
-      .then(() => {
-          res.redirect(`/places/${req.params.id}`)
-      })
-      .catch(err => {
-          console.log('err', err)
-          res.render('error404')
-      })
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+        res.redirect(`/places/${req.params.id}`)
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
+
 
 router.delete('/:id', (req, res) => {
-  db.Place.findByIdAndDelete(req.params.id)
-      .then(() => {
-          res.redirect('/places')
-      })
-      .catch(err => {
-          console.log('err', err)
-          res.render('error404')
-      })
+    db.Place.findByIdAndDelete(req.params.id)
+    .then(place => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
+
 router.get('/:id/edit', (req, res) => {
-  db.Place.findById(req.params.id)
-      .then(place => {
-          res.render('places/edit', { place })
-      })
-      .catch(err => {
-          res.render('error404')
-      })
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/edit', { place })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
+
 
 router.post('/:id/comment', (req, res) => {
   console.log(req.body)
